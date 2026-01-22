42c2510dffb6fa6049b3e8ad55d36551c79f8c43
Gustavo Martins <gufavmartins@hotmail.com>
2026-01-22 14:39:12 -0300

    Added BackHandler to return to the correct screen

--- app/src/standard/java/com/mekki/taco/presentation/ui/diet/ScanDietScreen.kt
+++ app/src/standard/java/com/mekki/taco/presentation/ui/diet/ScanDietScreen.kt
@@ -1,6 +1,7 @@
 package com.mekki.taco.presentation.ui.diet
 
 import android.graphics.Bitmap
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
@@ -13,6 +14,10 @@ fun ScanDietScreen(
     onPhotoCaptured: (Bitmap) -> Unit,
     onCancel: () -> Unit
 ) {
+    BackHandler {
+        onCancel()
+    }
+
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text("As funcionalidades IA não estão presentes nessa versão.")
     }
