92b0e1d8c2b5a9e2856701847463b29a2661e1f7
Gustavo Martins <gufavmartins@hotmail.com>
2026-01-25 23:21:16 -0300

    Bump app's version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.mekki.taco"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 6
+        versionName = "0.5.0-beta.rc2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
